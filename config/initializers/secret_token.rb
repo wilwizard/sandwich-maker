f32341d212b812ff182b881890760eb4855e2037
wilwizard <wilwizard4@gmail.com>
2013-08-16 14:37:27 -0500

    first commit blank rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SandwichMaker::Application.config.secret_token = '67b480ac64c57b487da9cde8968f847a9d871d968b8186667755ffc178a21252eb9ab4ebf06b77b76f69684d60dbf4579e059e067c85108736189ca5285f06f9'
