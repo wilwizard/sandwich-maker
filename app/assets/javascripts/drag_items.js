684417e3e77209f38b7ab348bac6f18c95d1964f
wilwizard <wilwizard4@gmail.com>
2013-08-16 23:14:51 -0500

    radio buttons added

--- app/assets/javascripts/drag_items.js
+++ app/assets/javascripts/drag_items.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
 	$(function(){
-		$('.pantry').sortable();
+		$('.pantry > *').draggable({
+			helper: "clone",
+    		containment: "document"
+		});
 		$('.pantry').disableSelection();
-	});
+		$('.sandwich').droppable({
+			drop: function(event, ui){
+				alert('dropped');
+				ui.draggable.detach().appendTo($(this));
+			},
+			tolerance: "touch"
+		});
 
-	$(function(){
-		$('.sandwich').droppable();
+
+
 	});
 });
