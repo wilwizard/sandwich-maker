6d7c5c8f2b544aae74bacf69837307393a5b0a72
wilwizard <wilwizard4@gmail.com>
2013-09-08 14:41:15 -0700

    spacing

--- app/models/sandwich.rb
+++ app/models/sandwich.rb
@@ -1,23 +1,23 @@
-class Sandwich < ActiveRecord::Base	
+class Sandwich < ActiveRecord::Base 
 
-	attr_accessible :sender, :receiver
+  attr_accessible :sender, :receiver
 
-	has_and_belongs_to_many :items
-	validates_presence_of :sender, :receiver
-	after_create :add_quote
+  has_and_belongs_to_many :items
+  validates_presence_of :sender, :receiver
+  after_create :add_quote
 
 
-	private
+  private
 
-	def add_quote
-		update_attribute(:quote, get_quote)		
-	end
+  def add_quote
+    update_attribute(:quote, get_quote)   
+  end
 
-	def get_quote
-		http = Typhoeus.get('http://www.iheartquotes.com/api/v1/random')
-		quote = http.body.split("\n")
-		quote.pop
-		quote.join('')
-	end
+  def get_quote
+    http = Typhoeus.get('http://www.iheartquotes.com/api/v1/random')
+    quote = http.body.split("\n")
+    quote.pop
+    quote.join('')
+  end
 
 end
